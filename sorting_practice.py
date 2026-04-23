9eec9a2908b2ec6db9da52784acbd16d053ba9a4
Alean Rodriguez <rodriguezalean005@gmail.com>
2026-04-22 20:48:09 -0400

    Selection sort implementation.

--- sorting_practice.py
+++ sorting_practice.py
@@ -1,9 +1,30 @@
+# Bubble Sort - O(n^2) time, O(1) space
+# Each pass bubbles the largest unsorted element to the end
+# i tracks passes, j walks adjacent pairs up to the already-sorted portion
 def bubble_sort(arr):
-    pass
+
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1], arr[j]
+
+    return arr
 
 
+# Selection Sort - O(n^2) time, O(1) space
+# Each pass finds the minimum in the unsorted portion and swaps it into position
+# i marks the boundary, tmp tracks the index of the current minimum
 def selection_sort(arr):
-    pass
+
+    for i in range(len(arr)):
+        tmp = i
+        for j in range(i + 1,len(arr)):
+            if arr[tmp] > arr[j]:
+                tmp = j
+        
+        arr[tmp],arr[i] = arr[i], arr[tmp]
+
+    return arr
 
 
 def insertion_sort(arr):
